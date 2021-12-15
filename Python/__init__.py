2bba6493baddd1861896caa125a0a1efb563537d
Wesly Backers <wesly.backers@gmail.com>
2021-12-15 22:40:21 +0100

    Added Comment

--- Python/__init__.py
+++ Python/__init__.py
@@ -1,12 +1,17 @@
 import sys
+
+# install pyshp with "pip install pyshp"
 import shapefile
 
+#get examples of shapefiles from https://www.diva-gis.org/datadown
 sf = shapefile.Reader("F:\Werk\Lichtenlijst\Data\BEL_water_lines_dcw.shp")
 
+#get data from the shapefile in records, fields and objecttypes
 records=sf.records()
 fields=sf.fields
 types = sf.shapeType
 
+#print results
 print(fields)
 print(types)
 print(records)
